a4d775d754a5d21b25296c8326c3b80dfb672b0b
slaxeea <kappelers77@gmail.com>
2021-11-24 17:27:23 +0100

    tabs

--- otter_list.dart
+++ otter_list.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'otter.dart';
 import 'main.dart';
 import 'otter_reader.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 class OtterList extends StatefulWidget {
   @override
@@ -10,9 +11,10 @@ class OtterList extends StatefulWidget {
 
 class _OtterListState extends State<OtterList> {
   List<Otter> otters;
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
 
   @override
-  void initState() {
+  Future<void> initState() {
     super.initState();
     OtterReader().read("data/otters.json").then((otters) {
       setState(() {
@@ -23,9 +25,32 @@ class _OtterListState extends State<OtterList> {
 
   @override
   Widget build(BuildContext context) {
+    int _selectedIndex = 0;
+
+    void _onItemTapped(int index) {
+      setState(() {
+        _selectedIndex = index;
+      });
+    }
+
     return Scaffold(
       appBar: AppBar(title: Text("Otter-Library")),
       body: otters == null ? buildWaitingWidget() : buildOtterWidget(),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Experience',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
     );
   }
 
@@ -42,11 +67,13 @@ class _OtterListState extends State<OtterList> {
     return ListTile(
         title: Text(otter.common),
         onTap: () {
-          toNextPage(Otter(otter.name, otter.common, otter.desc, otter.link, otter.imageUrl, otter.detail));
+          toNextPage(Otter(otter.name, otter.common, otter.desc, otter.link,
+              otter.imageUrl, otter.detail));
         });
   }
 
   void toNextPage(Otter otter) {
-    Navigator.push(context, MaterialPageRoute(builder: (_) => OtterPage(otter)));
+    Navigator.push(
+        context, MaterialPageRoute(builder: (_) => OtterPage(otter)));
   }
 }
