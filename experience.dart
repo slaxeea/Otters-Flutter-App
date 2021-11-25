214766d72aeab85b4154fef997bc5b641170c6b2
slaxeea <kappelers77@gmail.com>
2021-11-25 14:55:33 +0100

    added option to add new experience

--- experience.dart
+++ experience.dart
@@ -3,55 +3,69 @@ import 'package:flutter/material.dart';
 import 'styles.dart';
 import 'ExperienceClass.dart';
 
-Widget experience() {
-  List<ExperienceClass> experienceList = [
-  ];
+class experience extends StatefulWidget {
+  experience({Key key}) : super(key: key);
 
-  return Container(
-      child: Column(children: <Widget>[
-    Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Text("Experiences", style: title, textAlign: TextAlign.start),
-    ),
-    SizedBox(
-      height: 400,
-      child: ListView.builder(
-          itemCount: experienceList.length,
-          padding: const EdgeInsets.all(8),
-          itemBuilder: (BuildContext context, int index) {
-            return Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
-                child: Column(
-                  children: [
-                    Text(
-                      experienceList[index].title,
-                      style: text,
-                      textAlign: TextAlign.left,
-                    ),
-                    Text(
-                      experienceList[index].description,
-                      style: text,
-                      textAlign: TextAlign.start,
-                    ),
-                  ],
-                ),
-                decoration: boxdeco,
-              ),
-            );
-          }),
-    ),
-    Container(
-      child: FloatingActionButton(
-        onPressed: () {},
-        child: const Icon(Icons.add),
-        backgroundColor: Colors.lightBlue,
-        tooltip: "New",
+  @override
+  _experienceState createState() => _experienceState();
+}
+
+class _experienceState extends State<experience> {
+  @override
+  Widget build(BuildContext context) {
+    List<ExperienceClass> experienceList = [];
+
+    return Container(
+        child: Column(children: <Widget>[
+      Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Text("Experiences", style: title, textAlign: TextAlign.start),
       ),
-      alignment: Alignment.bottomRight,
-      padding: const EdgeInsets.only(right: 20),
-    ),
-  ]));
+      SizedBox(
+        height: 400,
+        child: ListView.builder(
+            itemCount: experienceList.length,
+            padding: const EdgeInsets.all(8),
+            itemBuilder: (BuildContext context, int index) {
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  child: Column(
+                    children: [
+                      Text(
+                        experienceList[index].title,
+                        style: text,
+                        textAlign: TextAlign.left,
+                      ),
+                      Text(
+                        experienceList[index].description,
+                        style: text,
+                        textAlign: TextAlign.start,
+                      ),
+                    ],
+                  ),
+                  decoration: boxdeco,
+                ),
+              );
+            }),
+      ),
+      Container(
+        child: FloatingActionButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => (newExperience())),
+            );
+          },
+          child: const Icon(Icons.add),
+          backgroundColor: Colors.lightBlue,
+          tooltip: "New",
+        ),
+        alignment: Alignment.bottomRight,
+        padding: const EdgeInsets.only(right: 20),
+      ),
+    ]));
+  }
 }
 
 class newExperience extends StatefulWidget {
@@ -62,6 +76,57 @@ class newExperience extends StatefulWidget {
 class _newExperienceState extends State<newExperience> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    void saveExperince(String title, String description) {
+      setState(() {
+        ExperienceClass experience = new ExperienceClass(title, description);
+      });
+    }
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("New Experience"),
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextField(
+                          decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: "Title",
+                      )),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextField(
+                          decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: "Description",
+                      )),
+                    ),
+                    Container(
+                      child: FloatingActionButton(
+                        onPressed: () {
+                          Navigator.pop(
+                            context
+                          );
+                        },
+                        child: const Icon(Icons.save),
+                        backgroundColor: Colors.lightBlue,
+                        tooltip: "Save",
+                      ),
+                      alignment: Alignment.bottomRight,
+                      padding: const EdgeInsets.only(right: 20),
+                    ),
+                  ],
+                ))
+          ],
+        ),
+      ),
+    );
   }
 }
